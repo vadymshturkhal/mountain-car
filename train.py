3c2ae83398821460835664ebff2a72195d7d2faa
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-20 21:22:51 +0300

    Added rewards.

--- train.py
+++ train.py
@@ -7,16 +7,21 @@ from n_step_off_policy_agent import NStepOffPolicyQLearning
 
 class TrainAgent:
     def __init__(self, epochs, is_rendering):
-        self.env = gym.make('MountainCar-v0', render_mode="human")
+        if is_rendering:
+            render_mode = "human"
+        else:
+            render_mode = "rgb_array"
+
+        self.env = gym.make('MountainCar-v0', render_mode=render_mode)
         self.car_agent =  NStepOffPolicyQLearning(*[is_load_weights, CAR_WEIGHTS_FILENAME, epochs, is_load_n_games])
         self.epochs = epochs
-        self._is_rendering = is_rendering
         self._car_game_reward = 0
         self._observations = []
         self._actions = []
         self._rewards = []
         self._dones = []
         self._losses = []
+        self._max_x = float('-inf')
 
     def train(self):
         for _ in range(self.epochs):
@@ -26,16 +31,17 @@ class TrainAgent:
     def _train_single_game(self):
         max_reward = float('-inf')
         prev_observation = self.env.reset()[0]
-
+        max_x = float('-inf')
         while True:
-            if self._is_rendering:
-                self.env.render()
-
             action = self.car_agent.get_action(prev_observation)
             action = np.argmax(action)
 
             # Step through the environment and get five return values
             observation, reward, terminated, truncated, info = self.env.step(action)
+
+            if prev_observation[0] > max_x:
+                max_x = prev_observation[0]
+                reward = 1
 
             self._observations.append(prev_observation)
             self._actions.append(action)
@@ -63,7 +69,7 @@ class TrainAgent:
                     self.car_agent.model.save(epoch=self.car_agent.n_games, filename=CAR_WEIGHTS_FILENAME)
 
                 average_loss = sum(self._losses) / len(self._losses)
-                print(average_loss)
+                print(average_loss, self.car_agent.epsilon)
                 break
 
     def _clear_game_data(self):
@@ -75,7 +81,10 @@ class TrainAgent:
         self._losses.clear()
 
 
-epochs = 10
+epochs = 100
+# is_rendering = False
+# is_load_weights = False
+# is_load_n_games = False
 is_rendering = True
 is_load_weights = False
 is_load_n_games = False
